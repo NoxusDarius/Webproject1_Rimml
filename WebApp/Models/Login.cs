3098ad74d9edbebd6bbd09269af4acdfd63ddf28
NoxusDarius <rmp.theghost@gmail.com>
2020-10-21 10:08:09 +0200

    Create Login.cs

---
+++ WebApp/Models/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Login
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string username { get; set; }
+
+        public Login() : this("", "", "") { }
+        public Login(string email, string password, string username)
+        {
+            
+            this.password = password;
+            this.username = username;
+            this.email = email;
+
+        }
+        public override string ToString()
+        {
+            return this.username + " "  + this.email + "\n";
+        }
+
+    }
+}
